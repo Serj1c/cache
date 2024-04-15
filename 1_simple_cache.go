a33ba1eda84069573bb250750e7c9effca74a1cd
Serj1c <sergej.a.isaev@gmail.com>
2024-04-16 00:24:10 +0300

    initial commit

---
+++ 1_simple_cache.go
@@ -0,0 +1,43 @@
+package cache
+
+import "sync"
+
+type SimpleCache struct {
+	data map[string]interface{}
+	mu   sync.RWMutex
+}
+
+func NewCache() *SimpleCache {
+	return &SimpleCache{
+		data: map[string]interface{}{},
+	}
+}
+
+func (c *SimpleCache) Get(key string) (value interface{}, ok bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	val, ok := c.data[key]
+	return val, ok
+}
+
+func (c *SimpleCache) Set(key string, val interface{}) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data[key] = val
+}
+
+func (c *SimpleCache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.data, key)
+}
+
+func (c *SimpleCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data = map[string]interface{}{}
+}
